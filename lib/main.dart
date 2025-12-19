af67f35a8b9382d3cf3ac6fde872a0fe0eb94751
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-12-19 16:30:56 +0200

    add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app_login_signup_pages/views/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
